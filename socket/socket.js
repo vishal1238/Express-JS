5dec2e30970aef6aaf9f9d4cf71131d2cef0c9cf
Vishal Gupta <webervishal@gmail.com>
2025-11-25 11:08:37 +0530

    update

--- socket/socket.js
+++ socket/socket.js
@@ -1,17 +1,38 @@
 import express from 'express';
-//create a server by http
+import { Server } from 'socket.io';
 import { createServer } from 'node:http';
 
 const app = express();
 
-//create server
+// create http server
 const server = createServer(app);
 
-//routes
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+// attach socket.io
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("new user connected:", socket.id);
+
+    // send welcome message
+    socket.emit("chatMessage", "Welcome to class chat");
+
+    // listen for message from client
+    socket.on("chatMessage", (msg) => {
+        console.log("msg from client:", msg);
+        io.emit("chatMessage", msg); // send to everyone
+    });
+
+    // disconnect event
+    socket.on("disconnect", () => {
+        console.log("user disconnected:", socket.id);
+    });
+});
+
+// routes
+app.get("/", (req, res) => {
+    res.send("<h1>Hello world</h1>");
 });
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+    console.log("server running at http://localhost:3000");
 });
